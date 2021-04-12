742a2e677f6a003932f42996831a719629b834b4
orlando-messias <programadorlando@gmail.com>
2021-04-12 16:49:59 -0300

    characterDetails and comicDetails works well with favorites comics and characters

--- src/Pages/CharacterDetails.jsx
+++ src/Pages/CharacterDetails.jsx
@@ -1,5 +1,9 @@
 // react
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+// redux
+import { useSelector, useDispatch } from 'react-redux';
+import { loginSuccess } from '../store/Login/Login.action';
 // react-icons
 import { IoChevronBackCircleSharp } from 'react-icons/io5';
 import { AiFillHome } from 'react-icons/ai';
@@ -8,29 +12,50 @@ import FoundCharacterCard from '../Components/FoundCharacterCard/FoundCharacterC
 import Topbar from '../Components/Topbar/Topbar';
 // services
 import api from '../services/marvelApi';
+import userApi from '../services/userApi';
+import { isLogin } from '../services/loginServices';
 // styles
 import './CharacterDetailsStyles.css';
-import { useHistory } from 'react-router';
 
 
 export default function CharacterDetails({ match }) {
   const [characterDetail, setCharacterDetail] = useState('');
+  const [isFavoriteCharacter, setIsFavoriteCharacter] = useState('');
 
   const name = match.params.name;
   const history = useHistory();
+  const dispatch = useDispatch();
+  const userId = useSelector(state => state.loginReducer.user.id);
 
+  // checks if user is logged in, if not, redirects to login page
+  useEffect(() => {
+    !isLogin()
+      ? history.push('/')
+      : dispatch(loginSuccess(JSON.parse(localStorage.getItem('loggedUser'))));
+  }, []);
+
+  // get character by name
   useEffect(() => {
     api().get(`&name=${name}`)
       .then(response => setCharacterDetail(response.data.data.results[0]));
   }, [name]);
 
+  // checks if character is favorite by the user
+  useEffect(() => {
+    userApi.get(`/favorites/characters/${userId}`)
+      .then(response => {
+        const isFavorite = response.data.some(ch => Number(ch.characterId) === characterDetail.id);
+        setIsFavoriteCharacter(isFavorite);
+      });
+  }, []);
+
   const handleBackClick = () => {
     window.history.back();
-  }
+  };
 
   const handleHomeClick = () => {
     history.push('/');
-  }
+  };
 
   return (
     <div className="container">
@@ -54,6 +79,7 @@ export default function CharacterDetails({ match }) {
 
         {characterDetail.thumbnail &&
           <FoundCharacterCard
+            id={characterDetail.id}
             name={characterDetail.name}
             description={characterDetail.description}
             image={`${characterDetail.thumbnail.path}.${characterDetail.thumbnail.extension}`}
@@ -61,6 +87,8 @@ export default function CharacterDetails({ match }) {
             series={characterDetail.series.available}
             stories={characterDetail.stories.available}
             events={characterDetail.events.available}
+            isFavoriteCharacter={isFavoriteCharacter}
+            setIsFavoriteCharacter={setIsFavoriteCharacter}
           />
         }
 
--- src/Pages/ComicDetails.jsx
+++ src/Pages/ComicDetails.jsx
@@ -1,42 +1,64 @@
 // react
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+// redux
+import { useSelector, useDispatch } from 'react-redux';
+import { loginSuccess } from '../store/Login/Login.action';
 // react-icons
-import { AiFillHome } from 'react-icons/ai';
+import { IoChevronBackCircleSharp } from 'react-icons/io5';
 import { AiOutlineHeart } from 'react-icons/ai';
 import { AiFillHeart } from 'react-icons/ai';
-import { useHistory } from 'react-router';
-import { Link } from 'react-router-dom';
 // components
 import Topbar from '../Components/Topbar/Topbar';
 // services
 import api from '../services/comicApi';
+import userApi from '../services/userApi';
+import { isLogin } from '../services/loginServices';
 // styles
 import './ComicDetailsStyles.css';
 
 
 export default function ComicDetails({ match }) {
   const [comicDetail, setComicDetail] = useState('');
-  const [favorite, setFavorite] = useState(false);
+  const [isFavoriteComic, setIsFavoriteComic] = useState(false);
 
-  const handleFavoriteClick = () => {
-    setFavorite(!favorite);
-  };
-
-  const id = match.params.id;
+  const comicId = match.params.id;
   const history = useHistory();
+  const dispatch = useDispatch();
+  const userId = useSelector(state => state.loginReducer.user.id);
 
+  // checks if user is logged in, if not, redirects to login page
   useEffect(() => {
-    api(id).get('/')
-      .then(response => setComicDetail(response.data.data.results[0]))
-  }, [id]);
+    !isLogin()
+      ? history.push('/')
+      : dispatch(loginSuccess(JSON.parse(localStorage.getItem('loggedUser'))));
+  }, []);
 
-  const handleBackContainerClick = () => {
+  // get comic details by comic id, plus verifies if comic is favorite or not
+  useEffect(() => {
+    api(comicId).get('/')
+      .then(response => setComicDetail(response.data.data.results[0]));
+
+    userApi.get(`/favorites/comics/${userId}`)
+      .then(response => {
+        const isFavorite = response.data.some(comic => comic.comicId === comicId);
+        setIsFavoriteComic(isFavorite);
+      });
+  }, [comicId]);
+
+  const handleBackClick = () => {
     window.history.back();
   };
 
   const handleCharacterClick = (name) => {
-    // console.log('character id ', id);
     history.push(`/characterdetails/${name}`);
+  };
+
+  const handleFavoriteClick = () => {
+    userApi.post('favorites/comics', { userId, comicId })
+      .then(() => {
+        setIsFavoriteComic(!isFavoriteComic);
+      });
   };
 
   return (
@@ -44,15 +66,12 @@ export default function ComicDetails({ match }) {
 
       <Topbar />
 
-      <Link to={'/'}>
-        <div className="backContainer" onClick={handleBackContainerClick}>
-          <div className="home">
-            <AiFillHome />
-            <span>home</span>
-          </div>
+      <div className="backContainer">
+        <div onClick={handleBackClick} className="backArrow">
+          <IoChevronBackCircleSharp />
+          <span>back</span>
         </div>
-
-      </Link>
+      </div>
 
       <div className="pageContainer">
         <h2>{comicDetail.title}</h2>
@@ -65,11 +84,11 @@ export default function ComicDetails({ match }) {
             />
           }
 
-          <div>
+          <div className="comicDetailsInfo">
             <div className="headerCard">
               <h3>{comicDetail.title}</h3>
               <span className="heart" onClick={handleFavoriteClick}>
-                {favorite ? <AiFillHeart style={{ color: 'red' }} /> : < AiOutlineHeart />}
+                {isFavoriteComic ? <AiFillHeart style={{ color: 'red' }} /> : < AiOutlineHeart />}
               </span>
             </div>
             <p>{comicDetail.description}</p>
