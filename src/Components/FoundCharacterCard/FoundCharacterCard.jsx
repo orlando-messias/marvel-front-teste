7cbdd84ebaf1636e38802a7c418edeeb3823ecf4
orlando-messias <programadorlando@gmail.com>
2021-04-12 14:18:32 -0300

    foundCharacterCard access endpoint to post favorite and not favorite

--- src/Components/FoundCharacterCard/FoundCharacterCard.jsx
+++ src/Components/FoundCharacterCard/FoundCharacterCard.jsx
@@ -1,23 +1,40 @@
 // react
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+// redux
+import { useSelector } from 'react-redux';
+// react-icons
 import { AiOutlineHeart } from 'react-icons/ai';
 import { AiFillHeart } from 'react-icons/ai';
+// services
+import userApi from '../../services/userApi';
 // styles
 import './FoundCharacterCardStyles.css';
 
 
 export default function FoundCharacterCard({
+  id,
   name,
   description,
   image,
   comics,
   series,
   stories,
-  events }) {
-  const [favorite, setFavorite] = useState(false);
+  events,
+  isFavoriteCharacter,
+  setIsFavoriteCharacter }) {
+
+  useEffect(() => {
+    isFavoriteCharacter ? setIsFavoriteCharacter(true) : setIsFavoriteCharacter(false);
+  }, []);
+
+  const userId = useSelector(state => state.loginReducer.user.id);
+  const characterId = id;
 
   const handleFavoriteClick = () => {
-    setFavorite(!favorite);
+    userApi.post('favorites/characters', { userId, characterId })
+      .then(() => {
+        setIsFavoriteCharacter(!isFavoriteCharacter);
+      })
   };
 
   return (
@@ -29,7 +46,7 @@ export default function FoundCharacterCard({
         <div className="headerCard">
           <h3>{name}</h3>
           <span className="heart" onClick={handleFavoriteClick}>
-            {favorite ? <AiFillHeart style={{color: 'red'}} /> : < AiOutlineHeart />}
+            {isFavoriteCharacter ? <AiFillHeart style={{ color: 'red' }} /> : < AiOutlineHeart />}
           </span>
         </div>
         <p>{description}</p>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,21 @@
 // react
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+// redux
+import { useSelector, useDispatch } from 'react-redux';
+import { loginSuccess } from '../store/Login/Login.action';
 // components
 import Topbar from '../Components/Topbar/Topbar';
+import SearchBar from '../Components/SearchBar/SearchBar';
 import CharacterCard from '../Components/CharacterCard/CharacterCard';
 import ComicCard from '../Components/ComicCard/ComicCard';
 import FoundCharacterCard from '../Components/FoundCharacterCard/FoundCharacterCard';
 // services
 import api from '../services/marvelApi';
+import userApi from '../services/userApi';
+import { isLogin } from '../services/loginServices';
 // styles
 import './HomeStyles.css';
-import SearchBar from '../Components/SearchBar/SearchBar';
 
 
 export default function Home() {
@@ -17,21 +23,37 @@ export default function Home() {
   const [character, setCharacter] = useState('');
   const [comics, setComics] = useState([]);
   const [search, setSearch] = useState(false);
+  const [isFavoriteCharacter, setIsFavoriteCharacter] = useState(false);
+
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const userId = useSelector(state => state.loginReducer.user.id);
 
   useEffect(() => {
+    // checks if user is logged in, if not, redirects to login page
+    !isLogin()
+      ? history.push('/')
+      : dispatch(loginSuccess(JSON.parse(localStorage.getItem('loggedUser'))));
+
     api().get('/')
       .then(response => setCharacters(response.data.data.results));
   }, []);
 
-  // get comics everytime a character is found
+  // get comics everytime a character is found, plus verifies if character is favorite or not
   useEffect(() => {
     if (character) {
       api(character.id).get(`/`)
         .then(response => setComics(response.data.data.results))
+
+      userApi.get(`/favorites/characters/${userId}`)
+        .then(response => {
+          const isFavorite = response.data.some(ch => Number(ch.characterId) === character.id);
+          setIsFavoriteCharacter(isFavorite);
+        });
     } else {
       setComics([]);
     }
-  }, [character]);
+  }, [character, isFavoriteCharacter]);
 
 
   return (
@@ -40,7 +62,7 @@ export default function Home() {
       <Topbar />
 
       <SearchBar setCharacter={setCharacter} setSearch={setSearch} />
-
+      {console.log(character && character.id)}
       <div className="pageContainer">
         {(!character && !search) &&
           <div className="cardContainer">
@@ -60,6 +82,7 @@ export default function Home() {
             <h2>Character</h2>
             <div className="foundCharacterCardContainer">
               <FoundCharacterCard
+                id={character.id}
                 name={character.name}
                 description={character.description}
                 image={`${character.thumbnail.path}.${character.thumbnail.extension}`}
@@ -67,6 +90,8 @@ export default function Home() {
                 series={character.series.available}
                 stories={character.stories.available}
                 events={character.events.available}
+                isFavoriteCharacter={isFavoriteCharacter}
+                setIsFavoriteCharacter={setIsFavoriteCharacter}
               />
             </div>
             <h2>Comics</h2>
