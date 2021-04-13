7d4f0411d2fa625b42c4a3214dfe9b5488c5ac7a
orlando-messias <programadorlando@gmail.com>
2021-04-13 16:07:42 -0300

    implements onclick on each character card available on home page

--- src/Components/CharacterCard/CharacterCard.jsx
+++ src/Components/CharacterCard/CharacterCard.jsx
@@ -1,12 +1,20 @@
 // react
 import React from 'react';
+import { useHistory } from 'react-router';
 // styles
 import './CharacterCardStyles.css';
 
 
 export default function CharacterCard({ name, id, image }) {
+
+  const history = useHistory();
+
+  const handleCharacterClick = (name) => {
+    history.push(`/characterdetails/${name}`);
+  };
+
   return (
-    <div className="characterCard">
+    <div className="characterCard" onClick={() => handleCharacterClick(name)}>
       <h3>{name}</h3>
       <img src={image} alt={name} />
     </div>
