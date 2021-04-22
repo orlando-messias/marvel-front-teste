9e32152c4f44918ece6e2ad1474fc01164a2e4ab
orlando-messias <programadorlando@gmail.com>
2021-04-22 09:42:03 -0300

    https

--- src/services/characterApi.js
+++ src/services/characterApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const timestamp = '1617803911';
 const apikey = 'aa99639576e1a4a3469c32aede4d16bd';
 const hash = 'e419cc9b5da7dce8bd014ec325efdf4b';
-const url = 'http://gateway.marvel.com/v1/public/characters';
+const url = 'https://gateway.marvel.com/v1/public/characters';
 const limit = 20;
 
 const characterApi = (characterId) => axios.create({
