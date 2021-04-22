ff4d4f6ac2f4b925b3f734c44bcafb35f0fd663b
orlando-messias <programadorlando@gmail.com>
2021-04-22 09:16:44 -0300

    http request heroku

--- src/services/userApi.js
+++ src/services/userApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const userApi = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'http://marvel-characters-comics.herokuapp.com/'
 });
 
 export default userApi;
